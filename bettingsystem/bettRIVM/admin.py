f6f70832b8f0d66bcb9d9b02d728cfe776cb34df
felipe-mr1 <felipemr698@gmail.com>
2025-03-20 22:01:10 -0300

    Register models in the admin panel

--- bettingsystem/bettRIVM/admin.py
+++ bettingsystem/bettRIVM/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Game, Bet, UserProfile, Transaction
 
-# Register your models here.
+admin.site.register(Game)
+admin.site.register(Bet)
+admin.site.register(UserProfile)
+admin.site.register(Transaction)
